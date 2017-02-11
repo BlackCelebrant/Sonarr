8c8e68bfbc57d4de2187b0af7cd43daf723de8de
Mark McDowall <markus.mcd5@gmail.com>
2017-02-10 22:47:11 -0800

    v3 UI

---
+++ frontend/src/Series/Editor/SeriesEditorFooterLabel.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { icons } from 'Helpers/Props';
+import SpinnerIcon from 'Components/SpinnerIcon';
+import styles from './SeriesEditorFooterLabel.css';
+
+function SeriesEditorFooterLabel(props) {
+  const {
+    label,
+    isSaving
+  } = props;
+
+  return (
+    <div className={styles.label}>
+      {label}
+
+      {
+        isSaving &&
+          <SpinnerIcon
+            className={styles.savingIcon}
+            name={icons.SPINNER}
+            isSpinning={true}
+          />
+      }
+    </div>
+  );
+}
+
+SeriesEditorFooterLabel.propTypes = {
+  label: PropTypes.string.isRequired,
+  isSaving: PropTypes.bool.isRequired
+};
+
+export default SeriesEditorFooterLabel;
